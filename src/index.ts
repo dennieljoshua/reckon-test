80287e3336be7b9020ded5f5b83b1a1c1d45520f
dennieljoshua <denniel.joshua@outlook.com>
2022-03-22 20:12:05 +0800

    :sparkles: initial implementation of test two

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,12 @@ interface Divisor {
   output: string;
 }
 
+interface TestTwoResponse {
+  candidate: string;
+  text: string;
+  results: Array<{ subtext: string; result: string }>;
+}
+
 app.get("/", async function testOne(req: Request, res: Response) {
   const range = await reckonClient.get("/test1/rangeInfo");
   const divisorsResponse = await reckonClient.get("/test1/divisorInfo");
@@ -25,6 +31,53 @@ app.get("/", async function testOne(req: Request, res: Response) {
   }
 
   return res.send(responseString);
+});
+
+app.post("/test2", async function testTwo(req: Request, res: Response) {
+  const { data: textData } = await reckonClient.get("/test2/textToSearch");
+  const { data: subTextData } = await reckonClient.get("/test2/subTexts");
+
+  const textToSearch = textData.text?.toLocaleLowerCase();
+
+  const resultsDictionary: { [subText: string]: Array<number | string> } = {};
+
+  for (const subText of subTextData.subTexts) {
+    const normalized: string = subText.toLocaleLowerCase();
+
+    for (let i = 0; i < textToSearch.length; i++) {
+      for (let j = 0; j < normalized.length; j++) {
+        const subChar = normalized[j];
+
+        if (subChar !== textToSearch[i + j]) {
+          break;
+        }
+
+        if (j === normalized.length - 1) {
+          console.log(`Matched ${normalized} starting at index: ${i}`);
+          if (!Array.isArray(resultsDictionary[subText])) {
+            resultsDictionary[subText] = [];
+          }
+          resultsDictionary[subText].push(i + 1);
+        }
+      }
+    }
+
+    // No match
+    if (!Array.isArray(resultsDictionary[subText])) {
+      resultsDictionary[subText] = ["<No Output>"];
+    }
+  }
+
+  const response: TestTwoResponse = {
+    candidate: "Denniel Joshua T. Diaz",
+    text: textData.text,
+    results: Object.entries(resultsDictionary).map(([key, value]) => ({
+      subtext: key,
+      result: value.toString(),
+    })),
+  };
+
+  return res.json(response);
 });
 
 app.listen(9999, () =>
